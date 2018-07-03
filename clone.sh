13a8a61ac742840ab9407bffe4008df7ea5b629d
ispardoa <kreijstal@github.com>
2018-07-03 00:03:12 -0500

    Add open-event-webapp

--- clone.sh
+++ clone.sh
@@ -3,3 +3,5 @@
 git clone --depth=1 https://github.com/fossasia/open-event-legacy v1
 git clone --depth=1 https://github.com/fossasia/open-event-server v2-server
 git clone --depth=1 https://github.com/fossasia/open-event-frontend v2-frontend
+git clone --depth=1 https://github.com/fossasia/open-event-webapp webapp
+
